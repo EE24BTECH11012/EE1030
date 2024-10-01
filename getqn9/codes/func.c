0225b119b41ae8e7cd2d3c39189b05f8aba87701
EE24BTECH11012 <bhavanisankargs@gmail.com>
2024-10-01 11:09:04 +0530

    write wtvr

--- getqn9/codes/func.c
+++ getqn9/codes/func.c
@@ -3,16 +3,16 @@
 
 double function(double x, double a)
 {
-	return 4*a*x ;
+	return 2*sqrt(a*x)  ;
 }
 
 double area(double lower_limit, double upper_limit)
 {
 	double sum=0 ;
-	double function(double, double) ;
-	for ( double i = lower_limit; i<=upper_limit; i+=0.01 )
+	//double function(double, double) ;
+	for ( double i = lower_limit; i<=upper_limit; i+=1e-7 )
 	{
-		sum += function(i+0.50, 1) ;
+		sum += function(i, 1)*1e-7 ;
 	}
 	return sum ;
 }
