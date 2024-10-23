c1e6005149f7ac02c46f9547e0bad3334a57e9d2
EE24BTECH11012 <bhavanisankargs@gmail.com>
2024-10-24 01:09:58 +0530

    write wtvr

--- getqn4/codes/func.c
+++
@@ -1,6 +0,0 @@
-#include <math.h>
-
-float mod( float point1[3], float point2[3], float point3[3]) 
-{
-	return sqrt(pow(point1[0]+point2[0]+point3[0], 2) + pow(point1[1]+point2[1]+point3[1], 2) + pow(point1[2]+point2[2]+point3[2], 2)) ;
-}
